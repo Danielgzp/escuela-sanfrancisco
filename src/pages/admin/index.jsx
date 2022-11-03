0d7200a375e1becab5377c7447082709db7e6186
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2022-11-03 18:53:18 -0400

    admin

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,8 +1,32 @@
+import Head from "next/head";
 import React from "react";
 import styles from "./admin.module.css";
 
 const index = () => {
-  return <div className={styles.content}>Admin Page</div>;
+  return (
+    <>
+      {/* <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <title>Edumin - Bootstrap Admin Dashboard </title>
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="images/favicon.png"
+        />
+        <link
+          rel="stylesheet"
+          href="vendor/bootstrap-select/dist/css/bootstrap-select.min.css"
+        />
+        <link rel="stylesheet" href="./styles css/style.css" />
+        <link rel="stylesheet" href="css/skin.css" />
+      </Head>
+      <body></body> */}
+    </>
+  );
 };
 
 export default index;
